2d35d2f30e09c9a56635a95f0e0c1d5aacb12be2
Haim Vales <haimvales@gmail.com>
2025-12-09 13:31:12 +0200

    number

--- utils/screen.js
+++ utils/screen.js
@@ -7,7 +7,7 @@ export function screen_printing(screen){
 }
 
 export function screen_launcher(){
-    return [['?','-','?'],['_','.','*'],['.','-','?']]
+    return [['0.0','1.0','2.0'],['0.1','1.1','2.1'],['0.2','1.2','2.2']]
 }
 
 export function edit_value(screen,x,y,val){
